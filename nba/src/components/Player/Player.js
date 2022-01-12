232d2058bd2c41853426f4f366be73c23e47bfcc
Jinhoo Bong <jinhoobong@gmail.com>
2022-01-12 14:21:21 -0800

    setting up data.json file

--- nba/src/components/Player/Player.js
+++ nba/src/components/Player/Player.js
@@ -14,20 +14,7 @@ const Player = () => {
   }, []);
 
   const getTeams = () => {
-    // axios({
-    //   method: 'GET',
-    //   url: 'https://api-nba-v1.p.rapidapi.com/teams/',
-    //   headers: {
-    //     'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
-    //     'x-rapidapi-key': '87b8929e7bmshe04f8ebb4c4ca49p1f669ajsn05e262630cb8'
-    //   }
-    // })
-    // .then((response) => {
-    //   console.log(response.data);
-    // })
-    // .catch((err) => {
-    //   console.log('team err', err);
-    // })
+  
     axios('data.json', {
       headers: {
           'Content-Type': 'application/json',
